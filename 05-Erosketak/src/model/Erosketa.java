fe788f01245251fd73950b47a20e4e8581a9de66
Basterra Badiola <basterra.alain@uni.lan>
2022-02-09 12:28:07 +0100

    09/02/2022

---
+++ 05-Erosketak/src/model/Erosketa.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author basterra.alain
+ */
+public class Erosketa {
+    protected char[] kodea;
+    protected LocalDate data;
+    protected Bezeroa bezeroa;
+    protected ArrayList<Produktua> produktuak = new ArrayList();
+    protected ArrayList<Integer> unitateak = new ArrayList();
+    protected double guztira;
+
+    public Erosketa(char[] kodea, Bezeroa bezeroa, double guztira) {
+        this.kodea = kodea;
+        this.bezeroa = bezeroa;
+        this.guztira = guztira;
+    }
+
+    @Override
+    public String toString() {
+        return "Erosketa{" + "kodea=" + kodea + ", bezeroa=" + bezeroa + ", guztira=" + guztira + '}';
+    }
+
+    @Override
+    public String toStringLuzea() {
+        return "Erosketa{" + "kodea=" + kodea + ", data=" + data + ", bezeroa=" + bezeroa + ", produktuak=" + produktuak + ", unitateak=" + unitateak + ", guztira=" + guztira + '}';
+    }
+
+    
+
+
+    
+}
